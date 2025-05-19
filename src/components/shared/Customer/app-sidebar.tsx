eef99c5a659cd8b694ec6398101cf5104bb41d97
Djonahuti <djonah04@gmail.com>
2025-05-19 19:04:39 +0100

    commit

--- src/components/shared/Customer/app-sidebar.tsx
+++ src/components/shared/Customer/app-sidebar.tsx
@@ -21,6 +21,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarRail,
+  useSidebar,
 } from "@/components/ui/sidebar"
 import { NavMain } from "./nav-main"
 import ThemeToggle from "@/components/ThemeToggle"
@@ -76,6 +77,7 @@ const data = {
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { state } = useSidebar()
   return (
     <Sidebar collapsible="icon" className="border-r-0" {...props}>
       <SidebarHeader>
@@ -86,9 +88,13 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               className="data-[slot=sidebar-menu-button]:!p-1.5">
               <a href="/">
                 <img
-                  src="/src/assets/ushop.svg"
+                  src={
+                    state === "collapsed" 
+                    ? "/src/assets/ushop-small.svg" 
+                    : "/src/assets/ushop.svg"
+                  }
                   alt="logo"
-                  width={85}
+                  width={state === "collapsed" ? 50 : 85}
                   height={20}
                   />
               </a>
